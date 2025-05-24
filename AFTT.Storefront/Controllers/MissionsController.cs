96aa8d4223229b1981811bc4913296db3bee06b7
Hutzpa <ilia.azek@gmail.com>
2025-05-24 21:35:58 +0300

    Add basic project structure and initial microservices

---
+++ AFTT.Storefront/Controllers/MissionsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AFTT.Storefront.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MissionsController : ControllerBase
+    {
+        //Get relevant user missions
+        //Add mission
+        //Add sidemission
+        //Update mission
+    }
+}
