e6d245023e40704aff682dfcba795ea362997d0e
Hutzpa <30867288+Hutzpa@users.noreply.github.com>
2025-06-03 17:18:32 +0300

    Check github web ide

--- AFTT.Common/Models/Request/Presentation/Missions/MissionCreateRequest.cs
+++ AFTT.Common/Models/Request/Presentation/Missions/MissionCreateRequest.cs
@@ -1,4 +1,9 @@
 ﻿namespace AFTT.Common.Models.Request.Presentation.Missions;
 public record MissionCreateRequest
 {
+    public required string Title { get; init; }
+    public string? Description { get; init; }
+    public DateTime PredictedCompletion { get; init; }
+    public MissionUrgency Urgency { get; init; }
+    public MissionStatus Status { get; init; }
 }
